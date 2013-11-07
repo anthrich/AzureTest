4940d2aebd7f3c129e3f5bdf16b4cf25b39cf9b8
anthrich83 <anthrblahblah@gmail.com>
2013-11-07 12:39:40 +0000

    Testing Azure connection strings.

--- AzureTest/AzureTest/Controllers/HomeController.cs
+++ AzureTest/AzureTest/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +12,9 @@ namespace AzureTest.Controllers
     {
         public ActionResult Index()
         {
+            SqlConnection sql = new SqlConnection(ConfigurationManager.ConnectionStrings["FreeDB20"].ConnectionString);
+            var cmd = new SqlCommand("select all from dbo.Department", sql);
+
             ViewBag.Message = "Modify! this template to jump-start your ASP.NET MVC application! THIS IS CHANGED NOW";
 
             return View();
