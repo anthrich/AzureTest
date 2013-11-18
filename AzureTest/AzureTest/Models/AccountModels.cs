7512e083ed634c35a6d3798d3944629d477cd8db
anthrich83 <anthrblahblah@gmail.com>
2013-11-18 11:24:00 +0000

    Fixed membership database connection string.

--- AzureTest/AzureTest/Models/AccountModels.cs
+++ AzureTest/AzureTest/Models/AccountModels.cs
@@ -11,7 +11,7 @@ namespace AzureTest.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            : base("DefaultConnection")
+            : base("FreeDB20")
         {
         }
 
